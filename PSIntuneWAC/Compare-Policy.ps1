16dbf3275ef60e51c5a32e2fae728d6ddf067d0a
Pouyan Khabazi <pkhabazi@outlook.com>
2019-08-06 16:22:19 +0200

    initial release of compare object

--- PSIntuneWAC/Compare-Policy.ps1
+++ PSIntuneWAC/Compare-Policy.ps1
@@ -1,28 +1,64 @@
-function Compare-Policy {
+function Compare-ObjectProperties {
     <#
     .SYNOPSIS
         coming soon
     .DESCRIPTION
         coming soon
-    .PARAMETER DeviceManagementPolicy
+    .PARAMETER ReferenceTemplate
         Coming soon
+    .PARAMETER DifferenceTemplate
+    Coming soon
     .EXAMPLE
-    Compare-Policy -DeviceManagementPolicy
+    Compare-ObjectProperties -ReferenceTemplate .\template.json -DifferenceTemplate .\template2.jos
+
     .NOTES
-    NAME: Compare-Policy
+    NAME: Compare-ObjectProperties
     #>
 
     [CmdletBinding()]
     param (
+        # Parameter help description
         [Parameter(Mandatory)]
-        [string]$DeviceManagementPolicy
+        [string]$ReferenceTemplate,
+
+        # Parameter help description
+        [Parameter(Mandatory)]
+        [string]$DifferenceTemplate
     )
 
-    begin {
-
-    }
-
     process {
-        Write-Output "$DeviceManagementPolicy"
+
+        try {
+            $DifferenceObject = (Get-Content $DifferenceTemplate | ConvertFrom-Json)
+        }
+        catch {
+            Write-Error $_ -ErrorAction Stop
+        }
+
+        try {
+            $ReferenceObject = (Get-Content $ReferenceTemplate | ConvertFrom-Json)
+        }
+        catch {
+            Write-Error $_
+        }
+
+        $objprops = $ReferenceObject | Get-Member -MemberType Property, NoteProperty | % Name
+        $objprops += $DifferenceObject | Get-Member -MemberType Property, NoteProperty | % Name
+        $objprops = $objprops | Sort | Select -Unique
+
+        $diffs = @()
+
+        foreach ($objprop in $objprops) {
+            $diff = Compare-Object $ReferenceObject $DifferenceObject -Property $objprop
+            if ($diff) {
+                $diffprops = @{
+                    PropertyName = $objprop
+                    RefValue     = ($diff | ? { $_.SideIndicator -eq '<=' } | % $($objprop))
+                    DiffValue    = ($diff | ? { $_.SideIndicator -eq '=>' } | % $($objprop))
+                }
+                $diffs += New-Object PSObject -Property $diffprops
+            }
+        }
+        if ($diffs) { return ($diffs | Select PropertyName, RefValue, DiffValue) }
     }
 }
