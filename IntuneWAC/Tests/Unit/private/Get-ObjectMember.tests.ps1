ef6bac61f7081a7e7c32465a443aaea202aea63e
PKhabazi <PKhabazi@outlook.com>
2019-08-16 13:20:33 +0200

    updating pipeline

--- IntuneWAC/Tests/Unit/private/Get-ObjectMember.tests.ps1
+++ IntuneWAC/Tests/Unit/private/Get-ObjectMember.tests.ps1
@@ -19,18 +19,12 @@ InModuleScope $moduleName {
     $object = New-Object psobject -Property $obj
 
     Describe Get-ObjectMember {
-        Mock -CommandName Write-Warning
-
         It 'Should return $true when a object was found' {
             Get-ObjectMember -Obj $object | Should -BeTrue
-
-            Assert-MockCalled -CommandName Write-Warning -Times 0 -Scope It
         }
 
         It 'Object count must be 2' {
             (Get-ObjectMember -Obj $object).count | Should -BeExactly 2
-
-            Assert-MockCalled -CommandName Write-Warning -Times 0 -Scope It
         }
     }
 }
